247d28b7db83c75f5c5b7487fa8ce5e576d82487
Yuwei Zhou <yuwzho@microsoft.com>
2017-07-29 19:48:38 +0800

    background url

--- src/Component/Welcome/Welcome.js
+++ src/Component/Welcome/Welcome.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 import './Welcome.css';
 
 class Welcome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: this.getImage(),
+            bodyWidth: document.body.clientWidth
+        };
+
+        this.getImage = this.getImage.bind(this);
+    }
+    
+    componentDidMount() {
+        window.addEventListener('resize', () => {
+            this.setState({
+                bodyWidth: document.body.clientWidth
+            });
+        }, false);
+    }
+
+    getImage() {
+        const bodyWidth = document.body.clientWidth;
+        var url = bodyWidth > 768 ? 'background_v.jpg' : 'background_h.jpg';
+        return process.env.PUBLIC_URL + '/img/' + url;
+    }
+
+
     render() {
+        const { image } = this.state;
+        console.log(image);
+        var style = {
+            backgroundImage: 'url(' + image + ')'
+        }
+
         return (
-            <div className="welcome">
+            <div className="welcome" style={style} >
                 <a href={this.props.scrollTo}><div className="intro-body">
                     <section><span id="arrow-0"></span></section>
                     <section><span id="arrow-1"></span></section>

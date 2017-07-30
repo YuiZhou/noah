58844a601ed741b98540797d1c4d4139eaa22769
Yuwei Zhou <yuwzho@microsoft.com>
2017-07-30 19:26:36 +0800

    smoothscroll

--- src/Component/Welcome/Welcome.js
+++ src/Component/Welcome/Welcome.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import smoothScroll from 'smoothscroll';
 import './Welcome.css';
 
 class Welcome extends Component {
@@ -10,37 +11,42 @@ class Welcome extends Component {
         };
 
         this.getImage = this.getImage.bind(this);
+        this.scrollTo = this.scrollTo.bind(this);
     }
-    
+
     componentDidMount() {
         window.addEventListener('resize', () => {
             this.setState({
-                bodyWidth: document.body.clientWidth
+                bodyWidth: document.body.clientWidth,
+                image: this.getImage()
             });
         }, false);
     }
 
     getImage() {
         const bodyWidth = document.body.clientWidth;
-        var url = bodyWidth > 768 ? 'background_v.jpg' : 'background_h.jpg';
+        var url = bodyWidth > 768 ? 'background_h.jpg' : 'background_v.jpg';
         return process.env.PUBLIC_URL + '/img/' + url;
     }
 
+    scrollTo() {
+        console.log('hello');
+        var to = document.querySelector(this.props.scrollTo);
+        smoothScroll(to);
+    }
 
     render() {
         const { image } = this.state;
-        console.log(image);
         var style = {
             backgroundImage: 'url(' + image + ')'
         }
 
         return (
-            <div className="welcome" style={style} >
-                <a href={this.props.scrollTo}><div className="intro-body">
+            <div className="welcome" style={style} onClick={this.scrollTo}>
+                <div className="intro-body">
                     <section><span id="arrow-0"></span></section>
                     <section><span id="arrow-1"></span></section>
                 </div>
-                </a>
             </div>
         );
     }
